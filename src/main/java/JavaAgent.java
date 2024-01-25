79e6005631e664bd45f067ddda72234fefcdd03c
Humphrey Burchell <humphreyburchell@gmail.com>
2024-01-25 11:26:48 +0000

    buffer is got to by method

--- src/main/java/JavaAgent.java
+++ src/main/java/JavaAgent.java
@@ -30,7 +30,7 @@ public class JavaAgent {
         // Thread printingHook = new Thread(() -> BuboCache.print());
         // Runtime.getRuntime().addShutdownHook(printingHook);
 
-        Thread writingHook = new Thread(() -> BuboPrinter.printPercentageBar(BuboPrinter.orderDataByTime(BuboDataReader.convertToHashMap(timeCache.Buffer, timeCache.pointer)), methodCache.Buffer));
+        Thread writingHook = new Thread(() -> BuboPrinter.printPercentageBar(BuboPrinter.orderDataByTime(BuboDataReader.convertToHashMap(timeCache.Buffer, timeCache.pointer)), methodCache.getBuffer()));
         Runtime.getRuntime().addShutdownHook(writingHook);
 
 
