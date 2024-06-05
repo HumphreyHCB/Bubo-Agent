49c805d363e8a458da943c8390a7b83147c4d59a
Humphrey Burchell <humphreyburchell@gmail.com>
2024-06-05 20:25:44 +0100

    TMP Priniting only

--- src/main/java/JavaAgent.java
+++ src/main/java/JavaAgent.java
@@ -21,73 +21,55 @@ public class JavaAgent {
      */
     public static void premain(String agentArgs, Instrumentation instrumentation) throws InstantiationException {
 
-        System.out.println("Starting Bubo Agent......");
+        System.out.println("Starting Bubo Agent...... From Agent");
         
         BuboCache timeCache = new BuboCache();
         timeCache.start();
-        // try {
-        // while(timeCache.pointer != 1){
-        //     Thread.sleep(100);
-        // }
-        // System.out.println("Time Cache Ready");
-        // } catch (InterruptedException e) {
-        //     // TODO Auto-generated catch block
-        //     e.printStackTrace();
-        // }
 
-        BuboMethodCache methodCache = new BuboMethodCache();
-        methodCache.start();
+        //BuboMethodCache methodCache = new BuboMethodCache();
+        //methodCache.start();
 
-        BuboCompUnitCache compunitCache = new BuboCompUnitCache();
-        compunitCache.start();
-
-        // try {
-        // while(methodCache.pointer != 0){
-        //     Thread.sleep(100);
-        // }
-        // System.out.println("method Cache Ready");
-        // } catch (InterruptedException e) {
-        //     // TODO Auto-generated catch block
-        //     e.printStackTrace();
-        // }
-
-
-        long startTime = System.currentTimeMillis();
-        // Thread printingHook = new Thread(() -> BuboCache.print());
-        // Runtime.getRuntime().addShutdownHook(printingHook);
+        //BuboCompUnitCache compunitCache = new BuboCompUnitCache();
+        //compunitCache.start();
 
         Thread writingHook = new Thread(() -> {
             System.out.println("Bubo Agent Joining......");
-            try {
-                timeCache.join();
-                methodCache.join();
-                compunitCache.join();
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
+            // try {
+            //     //timeCache.join();
+            //     //BuboMethodCache.join();
+            //     //compunitCache.join();
+            // } catch (InterruptedException e) {
+            //     // TODO Auto-generated catch block
+            //     e.printStackTrace();
+            // }
             System.out.println("Bubo Agent Starting Printing......");
             // long endTime = System.currentTimeMillis();
 
-            if (BuboMethodCache.pointer == 0) {
-                System.out.println("Method Cache is empty, did you forget to enable the profiler");
-                System.out.println("Add the follwoing command : -Dgraal.EnableProfiler=true ");
-                boolean didSOmthing = false;
-                for (int i = 0; i < 1000; i++) {
-                    if (BuboCache.Buffer[i] > 0) {
-                        didSOmthing = true;
+            // if (BuboMethodCache.pointer == 0) {
+            //     System.out.println("Method Cache is empty, did you forget to enable the profiler");
+            //     System.out.println("Add the follwoing command : -Dgraal.EnableProfiler=true ");
+            //     int foundMethods = 0;
+            //     for (int i = 0; i < 200000; i++) {
+            //         if (BuboCache.TimeBuffer[i] > 0) {
+            //             foundMethods++;
+            //         }
+            //     }
+            //     if (foundMethods > 0) {
+            //         System.out.println("We did find at least one method entry in the raw cache; therefore, something was recorded, but we don't know which method it belongs to.");
+            //         System.out.println("We found in the TimeBuffer :" + foundMethods + " Methods");
+            //     }
+                
+            // }
+            // else{
+                for (int i = 0; i < BuboCache.TimeBuffer.length; i++) {
+                    if (BuboCache.TimeBuffer[i] != 0) {
+                        System.out.println("Method Id : " + i + " AMOUNT : " + BuboCache.TimeBuffer[i]);
                     }
                 }
-                if (didSOmthing) {
-                    System.out.println("We did find at least one method entry in the raw cache; therefore, something was recorded, but we don't know which method it belongs to.");
-                }
-                
-            }
-            else{
              //BuboPrinter.printPercentageBar(BuboCache.Buffer, BuboMethodCache.getBuffer(), endTime - startTime );
              //BuboPrinter.printMultiBufferDebug(BuboCache.TimeBuffer,BuboCache.ActivationCountBuffer,BuboCache.CyclesBuffer, BuboMethodCache.getBuffer(), agentArgs);
-             BuboPrinter.printCompUnit(BuboCache.TimeBuffer,BuboCache.ActivationCountBuffer,BuboCache.CyclesBuffer, BuboMethodCache.getBuffer(), agentArgs, compunitCache.Buffer); 
-            }
+             ///BuboPrinter.printCompUnit(BuboCache.TimeBuffer,BuboCache.ActivationCountBuffer,BuboCache.CyclesBuffer, BuboMethodCache.getBuffer(), agentArgs, BuboCompUnitCache.Buffer);
+            //}
 
             //BuboPrinter.addToFile("VisualVM Run Count : " + BuboMethodCache.getBuffer().size());
             System.out.println("Bubo Agent Sutting Down......");
